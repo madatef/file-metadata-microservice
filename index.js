5a17b1da678e4d4ffc144253e688dfee0b7d4147
Mohammad Atef <madatef1@outlook.com>
2024-12-29 15:34:58 +0000

    feat: add /api/filemetadata endpoint and configure multer to upload and read files from local storage

--- index.js
+++ index.js
@@ -1,14 +1,34 @@
-var express = require('express');
-var cors = require('cors');
+const express = require('express');
+const cors = require('cors');
+const multer = require('multer');
+
 require('dotenv').config()
 
-var app = express();
+const app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
+});
+
+// Multer Configuration
+const upload = multer({ storage: multer.memoryStorage() });
+
+// POST endpoint for file upload
+app.post('/api/filemetadata', upload.single('upfile'), (req, res) => {
+  if (!req.file) {
+    return res.status(400).json({ error: 'No file uploaded' });
+  }
+
+  const { originalname, mimetype, size } = req.file;
+
+  res.json({
+    name: originalname,
+    type: mimetype,
+    size,
+  });
 });
 
 
